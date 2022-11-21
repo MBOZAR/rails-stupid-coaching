fef0d3e5962a43fdbd76a5924bbb6551b1044ba5
David Mboma <david.mboma.dm@gmail.com>
2022-11-21 16:11:34 -0500

    Playing around with bootstrap but can't figure it out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'questions#ask'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/ask", to: "questions#ask"
+  get '/ask', to: 'questions#ask'
   # Defines the root path route ("/")
   # root "articles#index"
   # root to: 'questions#answer'
-  get "/answer", to: 'questions#answer'
+  get '/answer', to: 'questions#answer'
 end
